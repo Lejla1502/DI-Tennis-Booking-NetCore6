dbd93c3db520fd0294f4bf5201e9945eb121f1fa
Lejla1502 <lejlasp@hotmail.com>
2022-11-20 15:48:09 +0100

    registering servie in Microsoft DI container

--- src/TennisBookings/Pages/Index.cshtml.cs
+++ src/TennisBookings/Pages/Index.cshtml.cs
@@ -4,6 +4,12 @@ namespace TennisBookings.Pages
 {
     public class IndexModel : PageModel
     {
+		private readonly IWeatherForecaster _weatherForecaster;
+
+		public IndexModel(IWeatherForecaster weatherForecaster)
+		{
+			_weatherForecaster = weatherForecaster;
+		}
 		public string WeatherDescription { get; private set; } =
             "We don't have the latest weather information right now, " +
 			"please check again later.";
@@ -14,11 +20,13 @@ namespace TennisBookings.Pages
 
         public async Task OnGet()
         {
-			var forecaster = new RandomWeatherForecaster();
+			//var forecaster = new RandomWeatherForecaster();
+
+
 
             try
             {
-                var currentWeather = await forecaster
+                var currentWeather = await _weatherForecaster
 					.GetCurrentWeatherAsync("Eastbourne");
 
                 switch (currentWeather.Weather.Summary)
