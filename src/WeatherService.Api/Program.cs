70263a831e4944c2831645298963381980fd04d9
Lejla1502 <lejlasp@hotmail.com>
2023-05-28 18:11:15 +0200

    DI with Minimal APIs

--- src/WeatherService.Api/Program.cs
+++ src/WeatherService.Api/Program.cs
@@ -1,6 +1,15 @@
+using TennisBookings.Shared.Weather;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IWeatherForecaster, RandomWeatherForecaster>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/weather/{city}", async (string city, IWeatherForecaster forecaster) =>
+{
+	var forecast = await forecaster.GetCurrentWeatherAsync(city);
+	return forecast.Weather;
+});
 
 app.Run();
